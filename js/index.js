d12de8acb42d68003ef1f20039c985b6330d4163
Scarlet <developerscarlet@gmail.com>
2020-06-06 20:30:03 -0400

    Load components on document ready

---
+++ js/index.js
@@ -0,0 +1,6 @@
+$(document).ready(() => {
+    console.log('ready')
+    $("#head_wrapper").load("../components/_head.html");
+    $("#navbar_wrapper").load("../components/_navbar.html");
+    $("#footer_wrapper").load("../components/_footer.html");
+})
