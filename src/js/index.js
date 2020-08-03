2575ca79be35dc5c955e772c6cb037589009a82d
Scarlet <developerscarlet@gmail.com>
2020-08-02 23:43:37 -0400

    WIP using jquery to load components

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,6 @@
-import './jquery.js'
-
 $(document).ready(() => {
     console.log('ready')
-    $("#head_wrapper").load("../components/_head.html");
+    $("#head_wrapper").load("\\components\\_head.html");
     $("#navbar_wrapper").load("../components/_navbar.html");
     $("#footer_wrapper").load("../components/_footer.html");
 })
