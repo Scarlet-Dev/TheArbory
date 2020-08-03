3462abea6895d34cd7bf7f5440771fb6132f4ad1
Scarlet <developerscarlet@gmail.com>
2020-08-02 20:38:01 -0400

    Remove async keywords and functions

--- gulpfile.js
+++ gulpfile.js
@@ -4,52 +4,53 @@ var csso = require('gulp-csso');
 var del = require('del');
 var gulp = require('gulp');
 var htmlmin = require('gulp-htmlmin');
+var imagemin = require('gulp-imagemin');
 var runSequence = require('run-sequence');
 var sass = require('gulp-sass');
 var uglify = require('gulp-uglify');
 
-function buildDist(){
-    Promise.all([compileScss, minifyJs, minifyHtml])
-    .then(console.log('Gulp build complete!'))
-    .catch(console.error.bind(console, 'There was an error when building the dist folder.'))
-}
+gulp.task('compileScss', function () {
+   return gulp.src('./src/sass/styles.scss')
+   .pipe(sass({
+       outputStyle: 'nested',
+       outFile: 'styles.css',
+       precision: 10,
+       onError: console.error.bind(console, 'Sass error:'),
+       onSuccess: console.log('Sass compile success!')
+   }))
+   .pipe(autoprefixer({browser: AUTOPREFIXER_BROWSERS}))
+   .pipe(csso())
+   .pipe(gulp.dest('./src/styles/css'))
+});
 
-async function compileScss(){
-    await gulp.task('styles', function () {
-        return gulp.src('./src/sass/styles.scss')
-        .pipe(sass({
-            outputStyle: 'nested',
-            outFile: 'style.css',
-            precision: 10,
-            onError: console.error.bind(console, 'Sass error:'),
-            onSuccess: console.log('Sass compile success!')
-        }))
-        .pipe(autoprefixer({browser: AUTOPREFIXER_BROWSERS}))
-        .pipe(csso())
-        .pipe(gulp.dest('./dist/css'))
-    })
-}
+gulp.task('compressCss', function() {
+    return gulp.src('./src/css/*.css')
+    .pipe()
+})
 
-async function minifyJs(){
-    await gulp.task('scripts', function() {
-        return gulp.src('./src/js/**/*.js')
-        .pipe(uglify({
-            output: console.log('')
-        }))
-        .pipe(gulp.dest('./dis/js'))
-    })
-}
+gulp.task('compressJs', function() {
+    return gulp.src('./src/js/**/*.js')
+    .pipe(uglify({
+        output: console.log('')
+    }))
+    .pipe(gulp.dest('./dis/js'))
+});
 
-async function minifyHtml(){
-    await gulp.task('pages', function() {
-        return gulp.src(['./src/**/*.html', './src/*.html'])
-        .pipe(htmlmin({
-            collapseWhitespace: true,
-            removeComments: true
-        }))
-        .pipe(gulp.dest('./dist'));
-    });
-}
+gulp.task('compressHtml', function() {
+    return gulp.src(['./src/**/*.html', './src/*.html'])
+    .pipe(htmlmin({
+        collapseWhitespace: true,
+        removeComments: true
+    }))
+    .pipe(gulp.dest('./dist'));
+});
+
+gulp.task('compressImages', function() {
+    return gulp.src(['./src/assets/*.+(png|jpg|jpeg|gif|svg)'])
+    .pipe()
+})
+
+gulp.task('buildDist')
 
 // Set the browser that you want to support
 const AUTOPREFIXER_BROWSERS = [
@@ -63,5 +64,3 @@ const AUTOPREFIXER_BROWSERS = [
     'android >= 4.4',
     'bb >= 10'
   ];
-
-exports.default = buildDist
