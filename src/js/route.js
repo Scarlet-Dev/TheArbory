1cf25ce2d0abc577bb6bb18f18998f5a8548105f
Scarlet <developerscarlet@gmail.com>
2020-08-09 21:07:57 -0400

    Conver route to es6 class

--- src/js/route.js
+++ src/js/route.js
@@ -1,31 +1,21 @@
-function Route(name, htmlName, defaultRoute){
-    try{
-        if(!name || !htmlName){
-            throw '';
-        }
-    }
-    catch (e){
-        console.error(e)
-    }
-}
+export class Route {
+    name;
+    htmlName;
+    defaultRoute;
 
-Route.prototype = {
-    name: undefined,
-    htmlName: undefined,
-    defaultRoute: undefined,
-
-    constructor: function(name, htmlName, defaultRoute){
+    constructor(name, htmlName, defaultRoute){    
         this.name = name;
         this.htmlName = htmlName,
         this.defaultRoute = defaultRoute
-    },
-    isActiveRoute: function(hashedPath){
-        return hashedPath.replace('#', '') === this.name
-    },
-
-    getParamsList: function(){
+    }    
+        
+    getParamsList(){
         var url = window.location.search;
         var urlParams = new URLSearchParams(url);
         return urlParams;
+    }
+    
+    isActiveRoute(hashedPath){
+        return hashedPath.replace('#', '') === this.name;
     }
 }
