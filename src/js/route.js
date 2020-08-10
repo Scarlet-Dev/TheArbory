eec9d6a866c132d7dcfec3ae02a32ff300bdfd24
Scarlet <developerscarlet@gmail.com>
2020-08-09 22:33:17 -0400

    Remove export; Add trycatch in construcotr init

--- src/js/route.js
+++ src/js/route.js
@@ -1,12 +1,20 @@
-export class Route {
+class Route {
     name;
     htmlName;
     defaultRoute;
 
-    constructor(name, htmlName, defaultRoute){    
-        this.name = name;
-        this.htmlName = htmlName,
-        this.defaultRoute = defaultRoute
+    constructor(name, htmlName, defaultRoute){ 
+        try{
+            if(!name || !htmlName){
+                throw 'error: name and htmlName params are mandatories'
+            }
+            this.name = name;
+            this.htmlName = htmlName,
+            this.defaultRoute = defaultRoute
+        }
+        catch (e){
+            console.error(e);
+        }
     }    
         
     getParamsList(){
